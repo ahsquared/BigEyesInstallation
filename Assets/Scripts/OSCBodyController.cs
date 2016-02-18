dfb16c03c0b3276fcb0578cddc6b63e7b7298ad9
Andre Hayter <ahayter@tombras.com>
2016-02-18 11:09:38 -0500

    fixed timer routine to work with GUI

--- Assets/Scripts/OSCBodyController.cs
+++ Assets/Scripts/OSCBodyController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
-using System.Timers;
+using System.Collections;
 
 public class OSCBodyController : MonoBehaviour {
 
@@ -11,44 +12,64 @@ public class OSCBodyController : MonoBehaviour {
 
     public int maxTime = 100;
     public int time;
-    public Timer timer;
+
+    private Text timerText;
 
     // Use this for initialization
     void Awake()
     {
         oscController = GameObject.Find("OSC").GetComponent<BigEyes.OSCController>();
         time = maxTime;
-        timer = new Timer(1000);
-        timer.Elapsed += (object sender, ElapsedEventArgs e) => time--;
-        timer.Stop();
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (time <= 0)
-        {
-            _bodySourceView.SetControlType("none");
-            gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
-            time = maxTime;
-            timer.Stop();
-            oscController.controllerActive = false;
-            //timer.Dispose();
-            //Destroy(gameObject);
-        }
-    }
+    //void Update()
+    //{
+    //    if (time <= 0)
+    //    {
+    //        _bodySourceView.SetControlType("none");
+    //        gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+    //        time = maxTime;
+    //        oscController.controllerActive = false;
+    //        //timer.Dispose();
+    //        //Destroy(gameObject);
+    //    }
+    //}
 
     void OnTriggerEnter(Collider other)
     {
-        if ((other.name == "HandLeft" || other.name == "HandRight") && !timer.Enabled && !oscController.controllerActive)
+        if ((other.name == "HandLeft" || other.name == "HandRight") && !oscController.controllerActive)
         {
             Debug.Log("hit hand");
+            if (timerText == null)
+            {
+                timerText = gameObject.GetComponentInChildren<Text>();
+                timerText.text = "ready";
+            }
+
             oscController.controllerActive = true;
             gameObject.GetComponent<Renderer>().material.color = new Color(1f, 0, 0, 0.5f);
             gameObject.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, 0));
             _bodySourceView.SetOSCPaths(oscPaths);
             _bodySourceView.SetControlType(controlType);
-            timer.Start();
+            StartCoroutine("handleTimerEvent", time);
         }
+    }
+
+    private IEnumerator handleTimerEvent(int time)
+    {
+        while (time > 0)
+        {
+            time--;
+            timerText.text = (time + 1).ToString();
+            yield return new WaitForSeconds(1);
+        }
+        _bodySourceView.SetControlType("none");
+        gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+        time = maxTime;
+        timerText.text = "";
+        oscController.controllerActive = false;
+
     }
 }
