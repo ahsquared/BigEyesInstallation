a653464c9eb6487329ffd8f206812b2a49a75b82
Andre Hayter <ahayter@tombras.com>
2016-03-28 02:04:14 -0400

    change hand color; add flare to buttons

--- Assets/Scripts/HighlightButton.cs
+++ Assets/Scripts/HighlightButton.cs
@@ -6,13 +6,18 @@ public class HighlightButton : MonoBehaviour {
     private bool _playing = false;
     private string _colliderName;
     private Material _mat;
-    private Color _originalColor;
-    private string _colorName = "_PrimaryColor";
+    private Color _primaryOriginalColor;
+    private Color _secondaryOriginalColor;
+    private string _primaryColorName = "_PrimaryColor";
+    private string _secondaryColorName = "_SecondaryColor";
+    private Light _light;
 
     // Use this for initialization
     void Start () {
         _mat = gameObject.GetComponent<Renderer>().material;
-        _originalColor = _mat.GetColor(_colorName);
+        _primaryOriginalColor = _mat.GetColor(_primaryColorName);
+        _secondaryOriginalColor = _mat.GetColor(_secondaryColorName);
+        if (!_light) _light = gameObject.GetComponent<Light>();
     }
 	
 	// Update is called once per frame
@@ -35,7 +40,11 @@ public class HighlightButton : MonoBehaviour {
             _colliderName = other.name;
 
             // highlight button
-            _mat.SetColor(_colorName, new Color(255f, 0f, 0f));
+            _mat.SetColor(_primaryColorName, new Color(255f, 0f, 216f));
+            _mat.SetColor(_secondaryColorName, new Color(255f, 0f, 216f));
+
+            // turn on flare
+            _light.intensity = 0.2f;
 
             // Fire a burst of particles
             gameObject.GetComponent<ParticleSystem>().Emit(20);
@@ -48,7 +57,9 @@ public class HighlightButton : MonoBehaviour {
         if ((other.name == _colliderName) && _playing)
         {
             _playing = false;
-            _mat.SetColor(_colorName, _originalColor);
+            _mat.SetColor(_primaryColorName, _primaryOriginalColor);
+            _mat.SetColor(_secondaryColorName, _secondaryOriginalColor);
+            _light.intensity = 0f;
         }
     }
 }
